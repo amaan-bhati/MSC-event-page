570b189fa941ac4cd0897b9dbad01b21af322289
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-01-13 17:03:38 +0530

    change exports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,21 @@
-import React from 'react'
-import { Suspense } from 'react'
-import { Canvas } from '@react-three/fiber'
-import Loader from '../components/Loader'
+import React from "react";
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
 
-{/*<div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-    popup
-  </div>*/}
 
 const Home = () => {
   return (
-   
-    <section className='w-full h-screen relative'>
-    <Canvas 
-    className='w-full h-screen relative'
-    camera={{near: 0.1, far: 1000 }}
-    >
-    <Suspense fallback={<Loader/>}>
-    </Suspense>{/*suspense loader will help the 3d model to load and while the 3d model is getting loaded it will show the lader*/}
-
-
-    </Canvas>
-
-
-
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen relative"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}></Suspense>
+        {/*suspense loader will help the 3d model to load and while the 3d model is getting loaded it will show the lader*/}
+      </Canvas>
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
