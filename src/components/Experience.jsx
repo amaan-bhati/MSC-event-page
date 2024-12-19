618f82fbea91facb4cb152e2a07772a7d246683b
Amaan Bhati <94218318+amaan-bhati@users.noreply.github.com>
2024-12-19 23:56:13 +0530

    feat: add-scene-component_edit-experience-component_add-scenes

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,9 +10,34 @@ import { useThree } from "@react-three/fiber";
 import { useAtom } from "jotai";
 import { useControls } from "leva";
 import { useEffect, useRef } from "react";
-//todo : add scene component here
+import { Scene } from "./Scene";
+
 export const scenes = [
-  //todo: create json formatted data for the scenes here
+  {
+    path: "models/cybertruck_scene.glb",
+    mainColor: "#f9c0ff",
+    name: "Cybertruck",
+    description:
+      "Better utility than a truck with more performance than a sports car",
+    price: 72000,
+    range: 660,
+  },
+  {
+    path: "models/model3_scene.glb",
+    mainColor: "#c0ffe1",
+    name: "Model 3",
+    description: "The car of the future",
+    price: 29740,
+    range: 576,
+  },
+  {
+    path: "models/semi_scene.glb",
+    mainColor: "#ffdec0",
+    name: "Semi",
+    description: "The Future of Trucking",
+    price: 150000,
+    range: 800,
+  },
 ];
 
 const CameraHandler = ({ slideDistance }) => {
